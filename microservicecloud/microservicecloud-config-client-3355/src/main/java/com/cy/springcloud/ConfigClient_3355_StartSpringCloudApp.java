dc3bf6ee9d3113831ce1e9ee876c1dc2e21bffca
崔岩 <cuiyan@travelsky.com>
2020-05-27 16:52:29 +0800

    测试

--- microservicecloud/microservicecloud-config-client-3355/src/main/java/com/cy/springcloud/ConfigClient_3355_StartSpringCloudApp.java
+++ microservicecloud/microservicecloud-config-client-3355/src/main/java/com/cy/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -2,7 +2,11 @@ package com.cy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 取配置文件信息
+ * @author lotus
+ *
+ */
 @SpringBootApplication
 public class ConfigClient_3355_StartSpringCloudApp {
     public static void main(String[] args) {
